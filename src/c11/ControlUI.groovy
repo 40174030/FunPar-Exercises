b0001e27a5c8ce6f4208f6b9ce1a283bf43459f9
Jack Yorston <40174030@live.napier.ac.uk>
2017-03-06 19:01:05 +0000

    Continued work on Exercise 11; UI implemented but not connected to system

--- src/c11/ControlUI.groovy
+++ src/c11/ControlUI.groovy
@@ -1,24 +1,40 @@
 package c11
 
 import org.jcsp.awt.*
+import org.jcsp.groovy.*
 import org.jcsp.lang.*
 import java.awt.*
 
 class ControlUI implements CSProcess {
 
-	def DisplayList thing
-	def int canvasSize
-	def ChannelInput x
-	def ChannelInput y
+	def ActiveCanvas canvas
+	def ChannelInput suspendButtonConfig
+	def ChannelInput scaleValueReset
+	def ChannelOutput scaleValueConfig
 	def ChannelOutput buttonEvent
 	
 	void run()
 	{
 		def root = new ActiveClosingFrame("Scaling")
 		def mainFrame = root.getActiveFrame()
-		def canvas = new ActiveCanvas()
+		mainFrame.setSize(100, 500)
 		
+		def suspendButton = new ActiveButton(suspendButtonConfig, buttonEvent, "Suspend")
+		def scaleLabel = new Label("Scale by:")
+		def scaleValue = new ActiveTextEnterField(scaleValueReset, scaleValueConfig, "")
+		def injectButton = new ActiveButton(null, buttonEvent, "Inject")
 		
+		def container = new Container()
+		container.setLayout(new GridLayout(1,5))
+		
+		mainFrame.setLayout(new BorderLayout())
+		mainFrame.add(canvas, BorderLayout.CENTER)
+		mainFrame.add(container, BorderLayout.SOUTH)
+		
+		mainFrame.pack()
+		mainFrame.setVisible(true)
+		def network = [root, canvas, scaleValue, suspendButton, injectButton]
+		new PAR (network).run()
 	}
 	
 }
